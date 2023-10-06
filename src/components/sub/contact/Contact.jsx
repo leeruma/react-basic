2903ad249f51cf2fd506d0a25fa131fd04be841b
leeruma <achieve4525@naver.com>
2023-10-06 13:19:09 +0900

    문의폼 키값 연결

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,19 +1,19 @@
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
+import emailjs from '@emailjs/browser';
 import { useRef, useEffect, useState } from 'react';
 
 export default function Contact() {
+	const form = useRef(null);
 	const map = useRef(null);
 	const view = useRef(null);
 	const instance = useRef(null);
 	const [Traffic, setTraffic] = useState(false);
-	const [Index, setIndex] = useState(0);
-	const [IsMap, setIsMap] = useState(true);
-
-	// 현재 kakao객체를 cdn으로 가져오고 있기 때문에
-	// 리액트 컴포넌트 안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
+	const [Index, setIndex] = useState(2);
+	const [IsMap, setIsMap] = useState(false);
 	const { kakao } = window;
-
+	//첫번째 지도를 출력하기 위한 객체정보
 	const info = useRef([
 		{
 			title: '삼성역 코엑스',
@@ -37,7 +37,7 @@ export default function Contact() {
 			imgPos: { offset: new kakao.maps.Point(116, 99) },
 		},
 	]);
-
+	//위의 정보값을 활용한 마커 객체 생성
 	const marker = new kakao.maps.Marker({
 		position: info.current[Index].latlng,
 		image: new kakao.maps.MarkerImage(
@@ -46,68 +46,93 @@ export default function Contact() {
 			info.current[Index].imgPos
 		),
 	});
-
 	const setCenter = () => {
 		// 지도 중심을 이동 시킵니다
 		instance.current.setCenter(info.current[Index].latlng);
 	};
-
 	useEffect(() => {
-		// Index 값이 변경될때마다 새로운 지도 레이어가 중첩되므로
-		// 일단은 기존 map안의 모든 요소를 없애서 초기화
+		//Index값이 변경될때마다 새로운 지도 레이어가 중첩되므로
+		//일단은 기존 map안의 모든 요소를 없애서 초기화
 		map.current.innerHTML = '';
-		//컴포넌트 마운트 되자마자 지도인스턴스 생성
+		//객체 정보를 활용한 지도 객체 생성
 		instance.current = new kakao.maps.Map(map.current, {
 			center: info.current[Index].latlng,
 			level: 1,
 		});
-		//마커 출력 인스턴스에 지도 인스턴스 결합
+		//마커 객체에 지도 객체 연결
 		marker.setMap(instance.current);
+		//지도 타입 변경 UI추가
 		const mapTypeControl = new kakao.maps.MapTypeControl();
 		instance.current.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMLEFT);
-
 		window.addEventListener('resize', setCenter);
-
 		//로드뷰 관련 코드
 		new kakao.maps.RoadviewClient().getNearestPanoId(
 			info.current[Index].latlng,
-			100, // 해당 지도의 위치값에서 반경 100미터 안에 제일 가까운 도로 기준으로 로드뷰화면 생성
+			100, //해당 지도의 위치값에서 반경 100미터 안에 제일 가까운 도로 기준으로 로드뷰화면 생성
 			(panoId) => {
-				new kakao.maps.Roadview(view.current).setPanoId(panoId, info.current[Index].latlng); //panoId와 중심좌표를 통해 로드뷰 실행
+				new kakao.maps.Roadview(view.current).setPanoId(panoId, info.current[Index].latlng);
 			}
 		);
-	}, [Index]); // Index 값이 변경될떄마다 지도화면이 다시 갱신되어야 하므로 Index값을 의존성 배열에 등록
-
+	}, [Index]); //Index값이 변경될때마다 지도화면이 다시 갱신되어야 하므로 Index값을 의존성 배열에 등록
 	useEffect(() => {
-		// traffic 값이 바뀔떄마다 실행될 구문
+		//traffic 값이 바뀔때마다 실행될 구문
 		Traffic
 			? instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
+	//form mail 기능함수
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('service_d806ydq', 'template_ancnwhn', form.current, 'FecYAv5d_nsKxfjRg').then(
+			(result) => {
+				alert('문의내용이 메일로 발송되었습니다.');
+			},
+			(error) => {
+				alert('문의내용 전송에 실패했습니다.');
+			}
+		);
+	};
 	return (
 		<Layout title={'Contact'}>
-			{/* <button onClick={() => setTraffic(true)}>주변 교통정보 보기</button>
-			<button onClick={() => setTraffic(false)}>주변 교통정보 끄기</button> */}
-			<button onClick={() => setTraffic(!Traffic)}>
-				{Traffic ? '교통정보끄기' : '교통정보켜기'}
-			</button>
-
-			<button onClick={setCenter}>지도 위치 초기화</button>
-			<button onClick={() => setIsMap(!IsMap)}>{IsMap ? '로드뷰보기' : '지도보기'}</button>
-
-			<div className='container'>
-				<div className={`view ${IsMap ? '' : 'on'}`} ref={view}></div>
-				<div className={`map ${IsMap ? 'on' : ''}`} ref={map}></div>
+			<div id='mailBox'>
+				<form ref={form} onSubmit={sendEmail}>
+					<label>Name</label>
+					<input type='text' name='user_name' />
+					<label>Email</label>
+					<input type='email' name='user_email' />
+					<label>Message</label>
+					<textarea name='message' />
+					<input type='submit' value='Send' />
+				</form>
 			</div>
 
-			<ul>
-				{info.current.map((el, idx) => (
-					<li className={Index === idx ? 'on' : ''} key={idx} onClick={() => setIndex(idx)}>
-						{el.title}
-					</li>
-				))}
-			</ul>
+			<div id='mapBox'>
+				<button onClick={() => setTraffic(!Traffic)}>
+					{Traffic ? '교통정보 끄기' : '교통정보 켜기'}
+				</button>
+				<button onClick={setCenter}>지도 위치 초기화</button>
+				<button onClick={() => setIsMap(!IsMap)}>{IsMap ? '로드뷰보기' : '지도보기'}</button>
+				<div className='container'>
+					<div className={`view ${IsMap ? '' : 'on'}`} ref={view}></div>
+					<div className={`map ${IsMap ? 'on' : ''}`} ref={map}></div>
+				</div>
+				<ul>
+					{info.current.map((el, idx) => (
+						<li
+							className={Index === idx ? 'on' : ''}
+							key={idx}
+							onClick={() => {
+								setIndex(idx);
+								setIsMap(true);
+							}}
+						>
+							{el.title}
+						</li>
+					))}
+				</ul>
+			</div>
 		</Layout>
 	);
 }
