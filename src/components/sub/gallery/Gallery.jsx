c062cdcb42aa0d166e2642eb7bb145b6fe43fefd
leeruma <achieve4525@naver.com>
2023-10-10 14:46:43 +0900

    뭔가 달라짐

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,27 +1,25 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
-import { useEffect, useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Masonry from 'react-masonry-component';
-
 export default function Gallery() {
 	const refFrame = useRef(null);
 	const refInput = useRef(null);
 	const refBtnSet = useRef(null);
 	const [Pics, setPics] = useState([]);
-	const my_id = '199296342@N06';
 	const [Loader, setLoader] = useState(true);
-
+	const my_id = '164021883@N04';
 	const fetchData = async (opt) => {
 		setLoader(true);
 		refFrame.current.classList.remove('on');
 		let url = '';
-		const api_key = 'bbf48601ef45cb60f5bcfdb652b8bfa4';
+		const api_key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-
 		const num = 100;
-
+		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
+		//해당URL을 통해 받아지는 데이터로 달라짐
 		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
 		}
@@ -31,35 +29,31 @@ export default function Gallery() {
 		if (opt.type === 'search') {
 			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
 		}
-
 		const data = await fetch(url);
 		const json = await data.json();
 		if (json.photos.photo.length === 0) {
 			return alert('해당 검색어의 결과값이 없습니다.');
 		}
-		// 실제 데이터가 state에 담기는 순간 가상돔이 생성되는 순간
+		//실제 데이터가 state에 담기는 순간 가상돔이 생성되는 순간
 		setPics(json.photos.photo);
-
 		let count = 0;
 		const imgs = refFrame.current?.querySelectorAll('img');
-
 		imgs.forEach((img, idx) => {
 			img.onload = () => {
 				++count;
-				console.log(count);
+				console.log('현재 로딩된 img갯수', count);
 				if (count === imgs.length) {
-					console.log('모든 이미지 소스 랜더링 완료');
+					console.log('모든 이미지 소스 렌더링 완료!');
+					//모든 소스이미지라 렌더링완료되면 Loader값을 false로 바꿔서 로딩이미지 제거
 					setLoader(false);
 					refFrame.current.classList.add('on');
 				}
 			};
 		});
 	};
-
 	useEffect(() => {
 		fetchData({ type: 'user', id: my_id });
 	}, []);
-
 	return (
 		<Layout title={'Gallery'}>
 			<div className='searchBox'>
@@ -69,7 +63,6 @@ export default function Gallery() {
 						if (refInput.current.value.trim() === '') {
 							return alert('검색어를 입력하세요.');
 						}
-
 						fetchData({ type: 'search', tags: refInput.current.value });
 						refInput.current.value = '';
 					}}
@@ -78,13 +71,19 @@ export default function Gallery() {
 					<button>검색</button>
 				</form>
 			</div>
-
 			<div className='btnSet' ref={refBtnSet}>
 				<button
 					className='on'
 					onClick={(e) => {
-						// 각 버튼 클릭시 해당 버튼에 만약 on클래스가 있으면 이미 활성화 되어 있는 버튼이므로 return으로 종료해서 fetchData함수 호출 반지
+						//각 버튼 클릭시 해당 버튼에 만약 on클래스가 있으면 이미 활성화 되어 있는 버튼이므로 return으로 종료해서
+						//fetchData함수 호출 방지
 						if (e.target.classList.contains('on')) return;
+
+						//클릭한 버튼요소에 on이없으면 해당 버튼활성화
+						const btns = refBtnSet.current.querySelectorAll('button');
+						btns.forEach((btn) => btn.classList.remove('on'));
+						e.target.classList.add('on');
+
 						fetchData({ type: 'user', id: my_id });
 					}}
 				>
@@ -92,15 +91,22 @@ export default function Gallery() {
 				</button>
 				<button
 					onClick={(e) => {
-						// 각 버튼 클릭시 해당 버튼에 만약 on클래스가 있으면 이미 활성화 되어 있는 버튼이므로 return으로 종료해서 fetchData함수 호출 반지
+						//각 버튼 클릭시 해당 버튼에 만약 on클래스가 있으면 이미 활성화 되어 있는 버튼이므로 return으로 종료해서
+						//fetchData함수 호출 방지
 						if (e.target.classList.contains('on')) return;
+
+						//클릭한 버튼요소에 on이없으면 해당 버튼활성화
+						const btns = refBtnSet.current.querySelectorAll('button');
+						btns.forEach((btn) => btn.classList.remove('on'));
+						e.target.classList.add('on');
+
 						fetchData({ type: 'interest' });
 					}}
 				>
 					Interest Gallery
 				</button>
 			</div>
-
+			{/* Loader가 true일때에만 로딩 이미지 출력 */}
 			{Loader && (
 				<img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='loading' />
 			)}
@@ -121,7 +127,6 @@ export default function Gallery() {
 										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
 									/>
 									<h2>{data.title}</h2>
-
 									<div className='profile'>
 										<img
 											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
