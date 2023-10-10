349cc5109563929497989bbd6f95bc0a5f6eb8a3
leeruma <achieve4525@naver.com>
2023-10-10 09:20:47 +0900

    갤러리 카드 이미지 수정

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,7 +5,7 @@ export default function Gallery() {
 	const [Pics, setPics] = useState([]);
 	const api_key = 'bbf48601ef45cb60f5bcfdb652b8bfa4';
 	const method_interest = 'flickr.interestingness.getList';
-	const num = 500;
+	const num = 50;
 	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
 	useEffect(() => {
 		fetch(url)
