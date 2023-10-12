607d7ee43a72ddc3b36c312a3bb6df81826a1ffe
leeruma <achieve4525@naver.com>
2023-10-12 09:48:38 +0900

    상세페이지에  url로 params전달

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,17 +1,14 @@
 import Layout from '../../common/layout/Layout';
-import Modal from '../../common/modal/Modal';
 import './Youtube.scss';
 import { useEffect, useState } from 'react';
 export default function Youtube() {
 	const [Youtube, setYoutube] = useState([]);
-	const [IsModal, setIsModal] = useState(false);
-	const [Index, setIndex] = useState(0);
 
 	// async await로 동기화 코드를 좀 더 깔끔하게 정리
 	const fetchYoutube = async () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-		const pid = 'PLnBZ3Abl_z0NvoNbCbU2WV5-rkmzQsnwa';
+		const pid = 'PLnBZ3Abl_z0P_UBpTwJ6-ev9qUBGbvKSb';
 		const num = 7;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
@@ -32,22 +29,14 @@ export default function Youtube() {
 
 					return (
 						<article key={idx}>
-							<div className='titBox'>
-								<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
-							</div>
-
 							<div className='conBox'>
-								<p>{desc.length > 60 ? desc.substr(0, 180) + '...' : desc}</p>
+								<h2>{tit.length > 18 ? tit.substr(0, 18) + '...' : tit}</h2>
+
+								<p>{desc.length > 80 ? desc.substr(0, 80) + '...' : desc}</p>
 								<span>{date.split('T')[0].split('-').join('.')}</span>
 							</div>
 
-							<div
-								className='picBox'
-								onClick={() => {
-									setIndex(idx);
-									setIsModal(true);
-								}}
-							>
+							<div className='picBox'>
 								<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
 							</div>
 						</article>
@@ -55,14 +44,14 @@ export default function Youtube() {
 				})}
 			</Layout>
 
-			{IsModal && (
+			{/* {IsModal && (
 				<Modal setIsModal={setIsModal}>
 					<iframe
 						src={`https://www.youtube.com/embed/${Youtube[Index].snippet.resourceId.videoId}`}
 						title='youtube'
 					></iframe>
 				</Modal>
-			)}
+			)} */}
 		</>
 	);
 }
