b59f684ff847e3c4da1a9ff4117fd5dfe8e2d369
leeruma <achieve4525@naver.com>
2023-10-12 13:43:27 +0900

    갤러리 페이지 이슈사항 정리

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,28 +5,22 @@ import { useState, useEffect, useRef } from 'react';
 import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
-	const refFrame = useRef(null);
 	const refInput = useRef(null);
 	const refBtnSet = useRef(null);
 	const [Pics, setPics] = useState([]);
-	const [Loader, setLoader] = useState(true);
 	const [ActiveURL, setActiveURL] = useState('');
-	const [IsModal, setIsModal] = useState(false);
-	const [Fix, setFix] = useState(false);
 	const [IsUser, setIsUser] = useState(true);
+	const [IsModal, setIsModal] = useState(false);
 	const my_id = '199296342@N06';
 
+	//처음 마운트 데이터 호출 함수
 	const fetchData = async (opt) => {
-		let count = 0;
-		setLoader(true);
-		refFrame.current.classList.remove('on');
 		let url = '';
 		const api_key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-
-		const num = 10;
+		const num = 50;
 
 		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
@@ -44,26 +38,10 @@ export default function Gallery() {
 		if (json.photos.photo.length === 0) {
 			return alert('해당 검색어의 결과값이 없습니다.');
 		}
-
 		setPics(json.photos.photo);
-
-		const imgs = refFrame.current?.querySelectorAll('img');
-		console.log(imgs);
-
-		imgs.forEach((img) => {
-			img.onload = () => {
-				++count;
-				console.log('현재 로딩된 img갯수', count);
-				if (count === (Fix ? imgs.length / 2 - 1 : imgs.length - 2)) {
-					console.log('모든 이미지 소스 렌더링 완료!');
-					setLoader(false);
-					refFrame.current.classList.add('on');
-				}
-			};
-		});
 	};
 
-	// submit 이벤트 발생시 실행 할 함수
+	//submit이벤트 발생시 실행할 함수
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		setIsUser(false);
@@ -79,31 +57,31 @@ export default function Gallery() {
 		refInput.current.value = '';
 	};
 
-	// myGallery 클릭 이벤트 발생시 실행할 함수
+	//myGallery 클릭 이벤트 발생시 실행할 함수
 	const handleClickMy = (e) => {
 		setIsUser(true);
 		if (e.target.classList.contains('on')) return;
 
 		const btns = refBtnSet.current.querySelectorAll('button');
 		btns.forEach((btn) => btn.classList.remove('on'));
-
 		e.target.classList.add('on');
+
 		fetchData({ type: 'user', id: my_id });
 	};
 
-	// Interset Gallery 클릭 이벤트 발생시 실행할 함수
+	//Interest Gallery 클릭 이벤트 발생시 실행할 함수
 	const handleClickInterest = (e) => {
 		setIsUser(false);
 		if (e.target.classList.contains('on')) return;
 
 		const btns = refBtnSet.current.querySelectorAll('button');
 		btns.forEach((btn) => btn.classList.remove('on'));
-
 		e.target.classList.add('on');
+
 		fetchData({ type: 'interest' });
 	};
 
-	// profile 아이디 클릭시 실행할 함수
+	//profile 아이디 클릭시 실행할 함수
 	const handleClickProfile = (e) => {
 		if (IsUser) return;
 		fetchData({ type: 'user', id: e.target.innerText });
@@ -132,14 +110,7 @@ export default function Gallery() {
 					<button onClick={handleClickInterest}>Interest Gallery</button>
 				</div>
 
-				{Loader && (
-					<img
-						className='loading'
-						src={`${process.env.PUBLIC_URL}/img/loading.gif`}
-						alt='loading'
-					/>
-				)}
-				<div className='picFrame' ref={refFrame}>
+				<div className='picFrame'>
 					<Masonry
 						elementType={'div'}
 						options={{ transitionDuration: '0.5s' }}
@@ -166,15 +137,12 @@ export default function Gallery() {
 												src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
 												alt={data.owner}
 												onError={(e) => {
-													setFix(true);
-
 													e.target.setAttribute(
 														'src',
 														'https://www.flickr.com/images/buddyicon.gif'
 													);
 												}}
 											/>
-
 											<span onClick={handleClickProfile}>{data.owner}</span>
 										</div>
 									</div>
@@ -193,3 +161,8 @@ export default function Gallery() {
 		</>
 	);
 }
+
+/*
+	클릭한 버튼을 또 클릭했을때 같은 데이터를 불필요하게 또 다시 fetching요청하지 않도록 
+	클릭한 버튼에 on이 붙어있을때 함수 호출을 강제 중지
+*/
