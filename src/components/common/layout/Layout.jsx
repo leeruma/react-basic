ff22942e594a632572310bcd754a85b4a6e35654
leeruma <achieve4525@naver.com>
2023-10-19 10:30:15 +0900

    swiper 기본 연결

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -2,16 +2,29 @@
 	1. Layout.jsx를 왜 만들었는지 설명
 */
 import './Layout.scss';
+import { useEffect, useRef } from 'react';
+import { useSplitText } from '../../../hooks/useSplitText';
 
 export default function Layout({ title, children }) {
+	const refFrame = useRef(null);
+	const refTitle = useRef(null);
+	const splitText = useSplitText();
+
+	useEffect(() => {
+		splitText(refTitle, 0.1);
+		setTimeout(() => {
+			refFrame.current.classList.add('on');
+		}, 300);
+	}, []);
+
 	return (
-		<section className={`layout ${title}`}>
+		<section ref={refFrame} className={`layout ${title}`}>
+			<h1 ref={refTitle}>{title}</h1>
+			<div className='bar'></div>
+
 			<figure></figure>
 
-			<div className='content'>
-				<h1>{title}</h1>
-				{children}
-			</div>
+			{children}
 		</section>
 	);
 }
