2ddc9e50f7ba27010ca1b598b1917d2a0728219f
leeruma <achieve4525@naver.com>
2023-10-19 13:43:58 +0900

    Layout수정

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -2,12 +2,35 @@ import './Visual.scss';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { useState } from 'react';
 
 function Visual() {
 	const { data } = useSelector((store) => store.youtube);
+	const [Index, setIndex] = useState(0);
 	return (
 		<section className='visual'>
-			<Swiper slidesPerView={3} spaceBetween={50} loop={true} centeredSlides={true}>
+			<div className='titBox'>
+				<ul>
+					{data.map((tit, idx) => {
+						if (idx >= 5) return null;
+						return (
+							<li key={idx} className={idx === Index ? 'on' : ''}>
+								<h3>{tit.snippet.title}</h3>
+
+								<button>View Detail</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={50}
+				loop={true}
+				centeredSlides={true}
+				// swiper loop 기능을 적용하는 순간 실제 연결되어 있는 패널갯수보다 동적으로 패널이 생성되면서 일반적인 방법으로는 활성화 패널의 순서값을 구할 수 없기 때문에 아래와 같은 방법으로 순서값을 구함.
+				onSlideChange={(el) => setIndex(el.realIndex)}
+			>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -203,44 +203,6 @@ export default function Contact() {
 					</div>
 				</div>
 			</div>
-
-			<div className='textBox'>
-				<div className='conBox'>
-					<div>
-						<h3>Contact</h3>
-						<p>
-							<span>Lorem, ipsum.</span>
-							<span>Lorem, ipsum dolor.</span>
-							<span>Lorem, ipsum.</span>
-						</p>
-						<a href='#/Contact.jsx'>visit us</a>
-					</div>
-					<div>
-						<h3>Contact</h3>
-						<p>
-							<span>Lorem ipsum dolor sit.</span>
-							<span>Lorem, ipsum dolor.</span>
-							<span>Lorem, ipsum.</span>
-						</p>
-						<a href='#/Contact.jsx'>shop</a>
-						<a href='#/Contact.jsx'>today's event</a>
-					</div>
-					<div className='icon'>
-						<h3>Contact</h3>
-						<i class='fa-brands fa-square-instagram'></i>
-						<i class='fa-brands fa-facebook'></i>
-						<i class='fa-brands fa-youtube'></i>
-						<i class='fa-brands fa-square-twitter'></i>
-					</div>
-					<div className='bt'>
-						<h3>Contact</h3>
-						<span>
-							<input type='text' name='user_name' />
-							<button>Search</button>
-						</span>
-					</div>
-				</div>
-			</div>
 		</Layout>
 	);
 }
